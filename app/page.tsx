8d62e679ccac147c83370f14ca5ae79c62061e2d
Carter-Bardell-Munro <cartermunro11@gmail.com>
2023-09-05 15:40:22 +1200

    create intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Intro from '@/components/intro'
 import React from 'react'
 
 const Home = () => {
   return (
-    <main className=""></main>
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+    </main>
   )
 }
 
---
+++ components/intro.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+import { HiDownload } from 'react-icons/hi';
+
+const Intro = () => {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/sanfrancisco.jpg"
+              width={150}
+              height={150}
+              quality={95}
+              priority={true}
+              className="h-30 w-30 rounded-lg object-cover border-[0.35rem] border-white shadow-xl "
+              alt="Portrait of Carter with Golden Gate Bridge in the background"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 left-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-10 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hi, I&apos;m Carter.</span> I&apos;m a{' '}
+        <span className="font-bold">Full Stack Developer</span> based out of{' '}
+        <span className="font-bold">Auckland, NZ</span>. I enjoy working on a
+        range of different projects. Some of my strengths are{' '}
+        <span className="underline">React, TypeScript, and Next.js</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 transition"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 transition cursor-pointer border border-black/10" href="/CV.pdf" download>
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a className="bg-white px-7 p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] transition cursor-pointer border border-black/10" href="https://www.linkedin.com/in/carter-bardell-munro-b20a27123/" target="_blank">
+          <BsLinkedin />
+        </a>
+        <a className="bg-white px-7 p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] transition cursor-pointer border border-black/10" href="https://github.com/carter-bardellmunro" target="_blank">
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Intro;
