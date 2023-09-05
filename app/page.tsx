9e77d6f66212e83409d1671f335edc7ceab31c9c
Carter-Bardell-Munro <cartermunro11@gmail.com>
2023-09-05 12:42:32 +1200

    create header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main className=""></main>;
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className=""></main>
+  )
 }
+
+export default Home
